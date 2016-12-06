d5ef47b53f2873af1bb12f33fb099b73b6572f91
Davi Chaves <dchaves@usc.edu>
2016-12-05 23:20:36 -0800

    essa merda ta funcionando

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    var comics = [];
+    var url = 'https://xkcd-forum-api.herokuapp.com/random';
+    var myPromise = new Promise(function(resolve, reject) {
+      for (var i = 0; i < 9; i++) {
+        Ember.$.getJSON(url).then(function(result){
+          comics.pushObject(result);
+        });
+      }
+      resolve(comics);
+    }).then(function (myComics) {
+      return myComics;
+    });
+    return myPromise;
+  }
 });
