c518c5456b12c712341d73859f67fe3692cc3a64
Davi Chaves <dchaves@usc.edu>
2016-12-06 02:22:58 -0800

    post ready

--- app/routes/comic.js
+++ app/routes/comic.js
@@ -2,6 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return params.comic_id;
+    var comics = [];
+    var comicID = params.comic_id;
+    var url = 'https://xkcd-forum-api.herokuapp.com/comics/' + comicID;
+    var myPromise = new Promise(function(resolve, reject) {
+      Ember.$.getJSON(url).then(function(result){
+        comics.pushObject(result);
+      });
+      resolve(comics);
+    }).then(function (myComics) {
+      return myComics;
+    });
+    return myPromise;
   }
 });
